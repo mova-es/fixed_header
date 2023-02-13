51e42055cc61634f1fadad2c7107129ce2d33627
Elvira Magasumova <mova.es94@gmail.com>
2023-02-13 11:43:12 +0500

    initial commit

---
+++ data.js
@@ -0,0 +1,32 @@
+const dataProductsJson = `[
+  {
+    "name": "MANGO  PEOPLE  T-SHIRT",
+    "price": "300",
+    "color": "Red",
+    "size": "XL",
+    "quantity": "2",
+    "img": "img/img_1.jpg"
+  },  {
+    "name": "MANGO  PEOPLE  T-SHIRT",
+    "price": "300",
+    "color": "Red",
+    "size": "XL",
+    "quantity": "2",
+    "img": "img/img_2.jpg"
+  },  {
+    "name": "MANGO  PEOPLE  T-SHIRT",
+    "price": "300",
+    "color": "Red",
+    "size": "XL",
+    "quantity": "2",
+    "img": "img/img_3.jpg"
+  },
+  {
+    "name": "ELLERY X M'O CAPSULE",
+    "price": "52",
+    "color": "Black",
+    "size": "XL",
+    "quantity": "1",
+    "img": "img/img_4.jpg"
+  }
+]`
